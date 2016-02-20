e81299e3e83debb5c39ecde0bb1c5cb22b40488b
AdeebNqo <adeebnqo@gmail.com>
2016-02-20 18:42:06 +0200

    Added easy way to retreive books in shelf
    There is an issue labeled "no shelf api functions? #8"
    in the original package. It seemed like something I needed
    but I wasn't interested in figuring out that mess. I have
    added a simple method to retrive books in shelf. This method
    does not use most of the parameters listed in https://www.goodreads.com/api/index#reviews.list

--- goodreads/client.py
+++ goodreads/client.py
@@ -163,3 +163,8 @@ class GoodreadsClient():
         """Get a review"""
         resp = self.request("/review/show.xml", {'id': review_id})
         return GoodreadsReview(resp['review'])
+
+    def list_books(self, user_id, shelf):
+        resp = self.request("/review/list/%s.xml" % user_id,
+                            {'shelf': shelf})
+        return resp.get('books')
